b6ba8c4e3303ba4b5982c0e3c4c4efce607e8be6
bendik0329 <razvanbendik0329@gmail.com>
2022-10-06 15:30:17 -0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -3,6 +3,6 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   url: 'https://uat.kloudrealty.com/api',
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ["testKey1", "testKey2"]),
   },
 });
