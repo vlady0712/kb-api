67c72913f0627ee41954f5b40f58050e496d7e96
bendik0329 <razvanbendik0329@gmail.com>
2022-10-07 09:41:54 -0500

    Updated salt and readme.

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJLbG91ZCBSZWFsdHksIEluYy4iLCJVc2VybmFtZSI6InJhbHBoYnJpZGdlcyIsImV4cCI6MTY2NTE1MzUwMCwiaWF0IjoxNjY1MTUzNTAwfQ.omaNc5-3CzNoqX7griPlQsHMfjzc8cXEtGIXn8Fqjbo'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'W-]P$K^.Z7{QlHKmzOjE5`1u&?S,%S)vJfzkF!jdil74Q*Ru|b {7^jg9$t7OOh['),
   },
 });
